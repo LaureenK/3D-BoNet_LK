08ae3b99f4a21b0ae2ec5270d2877887c883aac8
LaureenK <laureen@rkfinanz.de>
2022-01-05 11:01:17 +0100

    d

--- helper_data_s3dis_LK.py
+++ helper_data_s3dis_LK.py
@@ -22,11 +22,8 @@ class Data_S3DIS:
         self.root_folder_4_traintest = dataset_path
         self.train_files = self.load_full_file_list(areas = train_areas)
         self.test_files = self.load_full_file_list(areas = test_areas)
-        #jeder Block gilt als File
         print('train files:', len(self.train_files))
-        print(self.train_files)
         print('test files:', len(self.test_files))
-        print(self.test_files)
 
         self.ins_max_num = Data_Configs.ins_max_num
         self.train_batch_size = train_batch_size
