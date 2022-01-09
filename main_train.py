898860f7cc95c3c1243bad8d7b86f217266f4f04
LaureenK <laureen@rkfinanz.de>
2022-01-09 13:00:46 +0100

    d

--- main_train.py
+++ main_train.py
@@ -70,14 +70,14 @@ def train(net, data):
 
 			###### full eval, if needed
 			#if ep%5==0 and i==total_train_batch_num-1:
-			print('Testing')
+
 			if i==total_train_batch_num-1:
 				print('Testing')
 				from main_eval import Evaluation
 				result_path = './log/test_res/' + str(ep).zfill(3)+'_'+test_areas[0] + '/'
 				#Evaluation.ttest(net, data, result_path, test_batch_size=20)
 				
-				#Evaluation.evaluation(dataset_path, train_areas, result_path)
+				Evaluation.evaluation(dataset_path, train_areas, result_path)
 				print('full eval finished!')
 			
 		logging.warning('End epoch %d' % ep)
